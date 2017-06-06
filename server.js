0d728f59ee89dacd4c5c2edff4a3d1574e37a4b0
xxporkoxx <diegomello@tokenlab.com.br>
2017-06-06 15:54:06 +0100

    hardcoded

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ var routes     = require('./routes');
 var mongoose = require("mongoose");
 
 mongoURI = 'mongodb://localhost/restdb';
-mongoose.connect(process.env.MOGODB_URI || mongoURI);
+//mongoose.connect(process.env.MOGODB_URI || mongoURI);
 
-console.log("MOGOLAB_URI: "+process.env.MONGOLAB_URI+"MOGODB_URI: "+ process.env.MONGODB_URI+"MOGO_URI: "+ mongoURI);
+//MOGODB_URI:
+mongoose.connect("mongodb://heroku_frcr5b76:algam1kphefimb149iis04400k@ds141098.mlab.com:41098/heroku_frcr5b76");
+
+//MONGODB_URI
+//mongoose.connect("mongodb://heroku_porko:td20129395@ds161931.mlab.com:61931/heroku_k62d4cjg");
+
+console.log("MONGODB_URI: " +process.env.MONGODB_URI);
+console.log("MOGODB_URI: " + process.env.MOGODB_URI);
+console.log("MOGO_URI: " + mongoURI);
  
 // express app will use body-parser to get data from POST
 app.use(bodyParser.urlencoded({ extended: true }));
